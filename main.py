7a582222de0a37b3a6f36af47c4a6bb5ccabd263
YuHo-chang <changhoward.cs07@nctu.edu.tw>
2023-03-18 18:19:01 +0800

    TODO show result

--- main.py
+++ main.py
@@ -69,35 +69,34 @@ class network():
         return self.pred_y
     
 
-    def backpropagation(self, y, pred_y):
+    def backward(self, y, pred_y):
         dy = self.derivative_MSE(y, pred_y)
         dz3 = self.derivative_sigmoid(self.pred_y)
         dz2 = self.derivative_sigmoid(self.z2)
         dz1 = self.derivative_sigmoid(self.z1)
 
-        self.d_l3 = self.z2.T @ (dz3 * dy)
-        self.d_l2 = self.z1.T @ (dz2 * ((dz3 * dy) @ self.output.T))
-        self.d_l1 = self.x.T @ (dz1 * ((dz2 * ((dz3 * dy) @ self.output.T)) @ self.l2.T))
+        d_l3 = self.z2.T @ (dz3 * dy)
+        d_l2 = self.z1.T @ (dz2 * ((dz3 * dy) @ self.output.T))
+        d_l1 = self.x.T @ (dz1 * ((dz2 * ((dz3 * dy) @ self.output.T)) @ self.l2.T))
 
+        self.l1 = self.l1 - self.lr * d_l1
+        self.l2 = self.l2 - self.lr * d_l2
+        self.l3 = self.l3 - self.lr * d_l3
 
-    def update(self):
-        self.l1 = self.l1 - self.lr * self.d_l1
-        self.l2 = self.l2 - self.lr * self.d_l2
-        self.l3 = self.l3 - self.lr * self.d_l3
     
-
+    @staticmethod
     def sigmoid(x):
         return 1.0/(1.0 + np.exp(-x))
 
-
+    @staticmethod
     def derivative_sigmoid(x):
         return np.multiply(x, 1.0 - x)
 
-
+    @staticmethod
     def MSE(y, pred_y):
         return np.mean(np.linalg.norm(y-pred_y))
     
-
+    @staticmethod
     def derivative_MSE(y, pred_y):
         return -2 * (y - pred_y) / len(y)
 
@@ -112,8 +111,46 @@ class network():
         pass
 
 
+def train(args):
+    if args.task == "linear":
+        x, y = generate_linear()
+    else:
+        x, y = generate_XOR_easy()
+
+    model = network(2, 1, 4, 4, args.lr)
+
+    losses = []
+    acc_list = []
+    for i in range(args.epoch):
+        pred_y = model.forward(x)
+        loss = model.MSE(y, pred_y)
+        losses.append(loss)
+        model.backward(y, pred_y)
+
+        acc = np.sum(np.where(pred_y > 0.5, 1, 0) == y) / len(y) * 100
+        acc_list.append(acc)
+        print(f"epoch {i+1} loss : {loss} acc(%) : {acc}")
+
+    return model
+
+
+def test(args, model):
+    if args.task == "linear":
+        x, y = generate_linear()
+    else:
+        x, y = generate_XOR_easy()
+
+    pred_y = model.forward(x)
+    acc = np.sum(np.where(pred_y > 0.5, 1, 0) == y) / len(y) * 100
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", type=str, default="linear", help="select task: linear, xor")
     parser.add_argument("--lr", type=float, default=0.1, help="set learning rate")
-    parser.add_argument("--activation", type=b)
+    parser.add_argument("-A", "--activation_function", type=str, default="sigmoid", help="select activation function: sigmoid, ReLU, None")
+    parser.add_argument("--epoch", type=int, default=1000, help="number of training epoch")
+
+    args = parser.parse_args() 
+    model = train(args)
+    test(args, model)
